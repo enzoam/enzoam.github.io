23cbceddd7e43e4c733bed370509885398ee38a6
enzoam <enzoam@gmail.com>
2021-03-20 18:33:20 -0300

    V1 Miniadventure

---
+++ miniadventure/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1616275877,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/aimspot-sheet0.png",
+		"images/peer-sheet0.png",
+		"images/peer-sheet1.png",
+		"images/peerlaser-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tilemap.png",
+		"images/scrool-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
