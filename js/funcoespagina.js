4759846668e73c74a2f23b9fb075a59aff4e51f6
Enzo A. Marchiorato <enzoam@gmail.com>
2024-12-27 18:40:07 -0300

    Mudanças estruturais

--- js/funcoespagina.js
+++ js/funcoespagina.js
@@ -1,13 +1,5 @@
-//alert ('teste');
-
-/*$(document).ready(function() {
-    $("#loadpopup").on("click", function() {
-        //alert ('teste');
-        $("#popupcontent").load("/slideshow.html");
-    });
-});*/
-
 function function_carrega_popup_imgs(game) {
+    //let actualfile = new Date().valueOf();
     document.getElementById("popup").style.display = "block";
     document.getElementById("popupcontent").innerHTML='<object style="width:100%; height: 90%;" type="text/html" data="slideshow.html?selgame=' + game + '"></object>';
 }
